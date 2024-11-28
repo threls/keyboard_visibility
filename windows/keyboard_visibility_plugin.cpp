388b2e0b8d9183e52abe5a22f07519549704ce81
Mark Bonnici <mb6792@gmail.com>
2024-11-28 16:43:14 +0100

    add header

--- windows/keyboard_visibility_plugin.cpp
+++ windows/keyboard_visibility_plugin.cpp
@@ -3,6 +3,7 @@
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
+#include <flutter/event_channel.h> // Add this line
 
 #include <map>
 #include <memory>
